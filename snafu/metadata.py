a745b021c34545b0fd135470c723c21ffbcecb27
Tzu-ping Chung <uranusjr@gmail.com>
2017-10-04 00:49:52 +0800

    Be a good player and always close reg keys

--- snafu/metadata.py
+++ snafu/metadata.py
@@ -1,38 +1,43 @@
+import contextlib
 import pathlib
 import winreg
 
 
-def get_python_key():
-    return winreg.OpenKey(
+@contextlib.contextmanager
+def open_python_key():
+    key = winreg.OpenKey(
         winreg.ConnectRegistry(None, winreg.HKEY_CURRENT_USER),
         r'Software\Python\PythonCore',
     )
+    yield key
+    winreg.CloseKey(key)
 
 
 def get_installed_version_names():
-    i = 0
-    names = []
-    python_key = get_python_key()
-    while True:
-        try:
-            names.append(winreg.EnumKey(python_key, i))
-        except OSError:
-            break
-        i += 1
-    return names
+    with open_python_key() as python_key:
+        count, _, _ = winreg.QueryInfoKey(python_key)
+        return [
+            winreg.EnumKey(python_key, i)
+            for i in range(count)
+        ]
 
 
 def is_installed(name):
-    try:
-        key = winreg.OpenKey(get_python_key(), name)
-    except FileNotFoundError:
-        return False
-    return True
+    with open_python_key() as python_key:
+        try:
+            key = winreg.OpenKey(python_key, name)
+        except FileNotFoundError:
+            return False
+        winreg.CloseKey(key)
+        return True
 
 
 def get_installation_root_path(name):
-    try:
-        key = winreg.OpenKey(get_python_key(), r'{}\InstallPath'.format(name))
-    except FileNotFoundError:
-        return None
-    return pathlib.Path(winreg.QueryValue(key, ''))
+    with open_python_key() as python_key:
+        try:
+            key = winreg.OpenKey(python_key, r'{}\InstallPath'.format(name))
+        except FileNotFoundError:
+            return None
+        path = pathlib.Path(winreg.QueryValue(key, ''))
+        winreg.CloseKey(key)
+        return path
