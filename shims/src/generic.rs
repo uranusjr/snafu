a155101fdade664001ac87b6ad85b3fab23aa765
Tzu-ping Chung <uranusjr@gmail.com>
2017-10-10 18:39:13 +0800

    Experiemental Rust-based launcher

---
+++ shims/src/generic.rs
@@ -0,0 +1,14 @@
+mod shims;
+
+fn main() {
+    match shims::run() {
+        Ok(status) => match status.code() {
+            Some(code) => std::process::exit(code),
+            None => std::process::exit(0),
+        },
+        Err(error) => {
+            println!("Command failed: {}", error);
+            std::process::exit(1)
+        },
+    };
+}
---
+++ shims/src/shims.rs
@@ -0,0 +1,65 @@
+use std;
+use std::error::Error;
+use std::fmt::{self, Formatter, Display};
+use std::io::ErrorKind;
+use std::io::prelude::*;
+use std::process::{Command, ExitStatus};
+use std::string::String;
+
+struct Shim {
+    command: String,
+    arguments: Vec<String>,
+}
+
+impl Shim {
+    fn load() -> std::io::Result<Shim> {
+        let exe = try! { std::env::current_exe() };
+        let p = exe.with_extension("shim");
+        let f = try! { std::fs::File::open(&p) };
+        let reader = std::io::BufReader::new(&f);
+
+        let mut lines = reader.lines();
+        let command = try! { match lines.next() {
+            Some(result) => result,
+            None => { return Err(std::io::Error::new(
+                    ErrorKind::InvalidData, "Could not read command"));
+            },
+        } };
+
+        let mut arguments = Vec::new();
+        for result in lines {
+            let arg = match result {
+                Ok(line) => line,
+                Err(_) => { continue; },
+            };
+            arguments.push(arg);
+        }
+
+        Ok(Shim {
+            command: command,
+            arguments: arguments,
+        })
+    }
+}
+
+impl Display for Shim {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        write!(f, "{} {}", self.command, self.arguments.join(" "))
+    }
+}
+
+pub fn run() -> Result<ExitStatus, String> {
+    let shim = match Shim::load() {
+        Ok(shim) => shim,
+        Err(err) => { return Err(String::from(err.description())); },
+    };
+    match Command::new(&shim.command).args(&shim.arguments).status() {
+        Ok(status) => Ok(status),
+        Err(_) => {
+            let mut s = String::from("failed to launch \"");
+            s.push_str(&shim.command);
+            s.push_str("\"");
+            return Err(s);
+        },
+    }
+}
